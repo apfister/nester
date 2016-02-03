9c0a2e357df3dff291e6277266d6a9cf3bc937ab
Adam Pfister <apfister@esri.com>
2016-02-03 10:47:53 -0600

    trying to build

--- dist/browser/nester.js
+++ dist/browser/nester.js
@@ -247,9 +247,9 @@ api.addClassesToCard = function(card, offset, nestingWidth){
     }else{
       width = Math.floor(width);
     }
-    console.log('**** NESTING WIDTH ' + nestingWidth + ' card width: ' + card.width + ' class: col-lg-' + width);
+    console.log('**** NESTING WIDTH ' + nestingWidth + ' card width: ' + card.width + ' class: col-md-' + width);
   }
-  card.classNames = 'card-debug col-lg-' + width;
+  card.classNames = 'card-debug col-md-' + width;
   card.minHeight = card.height * 60;
   if(offset){
     card.classNames = card.classNames + ' col-sm-offset-' + offset;
